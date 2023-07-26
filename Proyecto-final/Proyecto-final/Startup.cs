821abadab25abaf852555cbd0031cdcbbbc2cb28
Sasafg <saryfg@gmail.com>
2023-07-25 23:35:22 -0600

    act
    prueba

---
+++ Proyecto-final/Proyecto-final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_final.Startup))]
+namespace Proyecto_final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
